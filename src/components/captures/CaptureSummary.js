502f339d04d9785a07cb43e29a4e834ee67f1e46
Angeloss <devnull@localhost>
2019-11-07 13:01:17 -0600

    Update: Se muestra el nombre del usuario y titulo de mision en los summaries de la lista de capturas.

--- src/components/captures/CaptureSummary.js
+++ src/components/captures/CaptureSummary.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
 
-const CaptureSummary = ({capture}) => {
-  return (
-    <div className="card z-depth-0 project-summary">
-      <div className="card-content grey-text text-darken-3">
-        <span className="card-title ">{capture.mission}</span>
-        <span className="card-title ">{capture.userId}</span>
-        {/* <p>Posted by {user.creator}</p> */}
+class CaptureSummary extends Component {
+  state = {
+    username: "",
+    mission: ""
+  }
+
+  componentDidMount(){
+    getFirestore().get({ collection: "users", doc: this.props.capture.userId })
+      .then((doc) => {
+        if (doc != undefined && doc.data().username != null) {
+          this.setState({ username: doc.data().username})
+        }
+      })
+      .catch((error) => {
+
+        this.setState({ username:"Error - No Existe Usuario" })
+      });
+    getFirestore().get({ collection: "missions", doc: this.props.capture.mission })
+      .then((doc) => {
+        if (doc != undefined && doc.data().title.es != null) {
+          this.setState({ mission: doc.data().title.es })
+        }
+      })
+      .catch((error) => {
+        this.setState({ mission: "Error - No existe Mision" })
+      });
+  }
+
+  render()
+  {
+
+    return (
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title ">{this.state.mission}</span>
+          <span className="card-title ">{this.state.username}</span>
+          {/* <p>Posted by {user.creator}</p> */}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
 
 export default CaptureSummary
